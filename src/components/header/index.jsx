f879f8f266a5e2c415804c80e13cb2cd0c29cfd1
dnh <rootdnh@gmail.com>
2023-05-26 14:17:26 -0300

    feat: create fist commit

---
+++ src/components/header/index.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+// Components
+import Cart from "../cart/index";
+
+// Styles
+import * as Styles from "./styles";
+
+// Utilities
+import { loginUser, logoutUser } from "../../redux/user/actions";
+
+function Header() {
+  const [cartIsVisible, setCartIsVisible] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const { currentUser } = useSelector((state) => state.userReducer);
+
+  const handleCartClick = () => {
+    setCartIsVisible(true);
+  };
+
+  const handleLoginClick = () => {
+    dispatch(loginUser({ name: "Felipe Rocha", email: "felipe@rocha.com" }));
+  };
+
+  const handleLogoutClick = () => {
+    dispatch(logoutUser());
+  };
+
+  return (
+    <Styles.Container>
+      <Styles.Logo>Redux Shopping</Styles.Logo>
+      <Styles.Buttons>
+        {currentUser ? (
+          <div onClick={handleLogoutClick}>Sair</div>
+        ) : (
+          <div onClick={handleLoginClick}>Login</div>
+        )}
+
+        <div onClick={handleCartClick}>Carrinho</div>
+      </Styles.Buttons>
+
+      <Cart isVisible={cartIsVisible} setIsVisible={setCartIsVisible} />
+    </Styles.Container>
+  );
+}
+
+export default Header;
